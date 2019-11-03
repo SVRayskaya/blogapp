9dfca4216ca166492b6c1adad2be60a1196fdaff
SVRAyskaya <s.v.rayskaya@yandex.ru>
2019-11-03 19:17:09 +0400

    Django transfer of qa_site

---
+++ venv/qablog/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # post views
+
+    re_path(r'^$', views.post_list, name='post_list'),
+
+    # path(r'^$', views.PostListView.as_view(), name='post_list'),
+
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
+]
